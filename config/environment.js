dfac8d7ea961a91ce699bfc4ee415d8020eb4e76
Basant Singh <51873983+Basant18@users.noreply.github.com>
2023-05-29 22:27:48 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -8,14 +8,14 @@ const development = {
         port: 587,
         secure: false,
         auth:{
-            user: 'bsntsngh16@gmail.com',
-            pass: 'swhuxzeileadmdog'
+            user: 'email',
+            pass: 'password'
         }
     },
     session_cookie_key: 'blahblahblah',
-    google_client_id: "548658330072-3ik37cggmgfv6sljg082gcge6h56ef0d.apps.googleusercontent.com",
-    google_client_secret: "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",
-    google_call_back_url: "http://localhost:8001/user/auth/google/callback"
+    google_client_id: "your google client id",
+    google_client_secret: "your google client sercret",
+    google_call_back_url: "your google callback url"
 };
 
 module.exports = development;
